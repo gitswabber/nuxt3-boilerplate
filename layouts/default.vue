35ee6c8f9689577980f25e9d9c76b3409e3c6498
gitswabber <doutorking@gmail.com>
2022-02-27 21:46:14 +0900

    Create pages

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    This is default layout!!!
+    <slot />
+  </div>
+</template>
+
+<script>
+// This will also work in `<script setup>`
+definePageMeta({
+  // layout: "custom",
+});
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This is index page.
+    <br>
+    <NuxtLink to="/about">Go to about</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+name: "index"
+}
+definePageMeta({
+  layout: true,
+});
+</script>
+
+<style scoped>
+
+</style>
