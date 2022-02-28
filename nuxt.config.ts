3cea15561811092f2116b4156ead3cd75e3aeb00
swabber <doutorking@gmail.com>
2022-02-28 09:30:10 +0900

    Add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
     typescript : {
       shim: false
     },
+    buildModules: [
+        "@pinia/nuxt",
+        // '@nuxtjs/composition-api/module'
+    ],
     // plugins: ['~/plugins/vuetify.ts']
     css: ['vuetify/lib/styles/main.sass'],
     build: {
