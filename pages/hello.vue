05948ef975eb27c575d0f930769f8c991ea03532
swabber <doutorking@gmail.com>
2022-03-04 18:42:58 +0900

    Add test api route and middleware

---
+++ pages/hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Page visits: {{ data }}
+  </div>
+</template>
+
+<script setup>
+const { hello } = await useAsyncData('hello', () => $fetch('/api/hello'))
+</script>
+
+<style scoped>
+
+</style>
