dcec799ed49c173a444d779e5aa14af044200aca
swabber <doutorking@gmail.com>
2022-03-05 20:32:12 +0900

    Add server call test

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,6 +1,19 @@
-import type { IncomingMessage, ServerResponse } from 'http'
+/*import type { IncomingMessage, ServerResponse } from 'http'
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
     res.statusCode = 200
     res.end('Works!')
+}*/
+import axios from "axios";
+import runtimeConfig from "#config";
+
+export default async () => {
+    const result = await axios.get(runtimeConfig.api + '/hello')
+        .then((response) => {
+            console.log()
+            console.log(response)
+            return response
+        })
+    console.log(result.data)
+    return result.data
 }
