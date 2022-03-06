2b779170863163ee95638a912c03bb82c8579da0
swabber <doutorking@gmail.com>
2022-03-06 21:32:36 +0900

    Modify layout

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -3,8 +3,12 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="showSideBar = !showSideBar"></v-app-bar-nav-icon>
       <v-toolbar-title>
-        <NuxtLink to="/home">nuxt3 test app</NuxtLink>
+        <div @click="goHome">Hello Nuxt world</div>
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon to="/">
+        <v-icon>mdi-exit-run</v-icon>
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
@@ -12,7 +16,13 @@
 <script setup>
 import {useShowSideBar} from "../../composables/status";
 
+const router = useRouter()
+
 const showSideBar = useShowSideBar()
+
+function goHome() {
+  router.push({path: '/home'})
+}
 </script>
 
 <style scoped>
